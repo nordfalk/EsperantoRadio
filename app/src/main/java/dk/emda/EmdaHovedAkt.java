8c7fb9192b3b83296c5cf8f4a0cc608b5faa6b04
Emdadollah <emdah1@hotmail.com>
2017-03-16 15:34:40 +0100

    recycler view implementeret og mange andre klasser, fejl fikset i hovedaktivitet.

--- app/src/main/java/dk/emda/EmdaHovedAkt.java
+++ app/src/main/java/dk/emda/EmdaHovedAkt.java
@@ -1,5 +1,6 @@
 package dk.emda;
 
+import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -7,10 +8,30 @@ import android.os.Bundle;
 import dk.dr.radio.v3.R;
 
 public class EmdaHovedAkt extends AppCompatActivity {
+    private SectionsPageAdapter mSectionsPageAdapter;
+
+    private ViewPager mViewPager;
+
+    private static final String BUNDLE_EXTRAS = "BUNDLE_EXTRAS";
+    private static final String EXTRA_QUOTE = "EXTRA_QUOTE";
+    private static final String EXTRA_ATTR = "EXTRA_ATTR";
+
+    private static final String TAG = "MainActivity";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.emdah_activity_main);
+
+        mSectionsPageAdapter = new SectionsPageAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        setupViewPager(mViewPager);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(mViewPager);
 
 
     }

e45586ad92117f55b5222babe7a8003dc116b057
Statsminizzle <overload@live.dk>
2017-03-21 17:39:03 +0100

    cast tilføjet

--- app/src/main/java/dk/dk/niclas/NiclasHovedAkt.java
+++ app/src/main/java/dk/dk/niclas/NiclasHovedAkt.java
@@ -1,8 +1,10 @@
 package dk.dk.niclas;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import dk.dk.niclas.cast.VideoBrowserActivity;
 import dk.dr.radio.v3.R;
 
 
@@ -11,6 +13,8 @@ public class NiclasHovedAkt extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.niclas_hoved_akt);
+        //setContentView(R.layout.niclas_hoved_akt);
+
+        startActivity(new Intent(this, VideoBrowserActivity.class));
     }
 }

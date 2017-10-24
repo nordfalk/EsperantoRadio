291d9c8c31c9670363afe7420c19bc14a93ce74d
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2017-10-24 15:51:15 +0200

    Oprydning

--- app/src/main/java/dk/dr/radio/diverse/App.java
+++ app/src/main/java/dk/dr/radio/diverse/App.java
@@ -76,6 +76,7 @@ import dk.dr.radio.data.Kanal;
 import dk.dr.radio.data.Lydstream;
 import dk.dr.radio.data.Programdata;
 import dk.dr.radio.data.dr_v3.GammelDrRadioBackend;
+import dk.dr.radio.data.dr_v3.MuOnlineRadioBackend;
 import dk.dr.radio.data.esperanto.EoKanal;
 import dk.dr.radio.data.esperanto.EsperantoRadioBackend;
 import dk.dr.radio.net.Diverse;
@@ -161,7 +162,8 @@ public class App {
     }
 
     backend = Udseende.ESPERANTO ? new Backend[] { new EsperantoRadioBackend() }
-            : new Backend[] { new GammelDrRadioBackend(), new MuOnlineTVBackend(), new EsperantoRadioBackend(),  };
+//            : new Backend[] { new GammelDrRadioBackend(), new MuOnlineTVBackend(), new EsperantoRadioBackend(),  };
+            : new Backend[] { new MuOnlineRadioBackend(), new MuOnlineTVBackend(), new EsperantoRadioBackend(),  };
 
     sprogKonfig = new Configuration();
     sprogKonfig.locale = new Locale(!Udseende.ESPERANTO ? "da_DK" : "eo");
@@ -353,13 +355,13 @@ public class App {
       // kan der komme rigtig mange store anomdninger i kø
       //  - det gøres kun én gang, hvilket skulle dække de fleste scenarier
       // TODO den rigtige løsning burde være at svarene for Drama&Bog og A-Å bliver hængende i cachen, tjekket her burde være om de er i cachen eller ej
-      /*
       if (!Udseende.ESPERANTO && færdig && !prefs.getBoolean(DRAMA_OG_BOG__A_Å_INDLÆST, false)) {
         prefs.edit().putBoolean(DRAMA_OG_BOG__A_Å_INDLÆST, true);
         færdig = false;
         data.dramaOgBog.startHentData();
         data.programserierAtilÅ.startHentData();
       }
+      /*
       */
       if (færdig) {
         netværk.observatører.remove(this); // Hold ikke mere øje med om vi kommer online

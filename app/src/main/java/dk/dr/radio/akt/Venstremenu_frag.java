cf5f217e3063dcfa26258fceead2ec8de3857ae6
Jacob Nordfalk <jacob.nordfalk@gmail.com>
2018-09-06 17:38:56 +0200

    Opdatering af biblioteker

--- app/src/main/java/dk/dr/radio/akt/Venstremenu_frag.java
+++ app/src/main/java/dk/dr/radio/akt/Venstremenu_frag.java
@@ -14,8 +14,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
@@ -273,7 +273,7 @@ public class Venstremenu_frag extends Fragment implements Runnable {
   }
 
   private ActionBar getActionBar() {
-    return ((ActionBarActivity) getActivity()).getSupportActionBar();
+    return ((AppCompatActivity) getActivity()).getSupportActionBar();
   }
 
   public void visMenu() {
